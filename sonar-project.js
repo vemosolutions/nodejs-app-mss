369a6d03f90015ae2402fd64da87fb61ed95b59d
dsjayakar <34391658+dsjayakar@users.noreply.github.com>
2021-03-09 08:04:44 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,13 +1,13 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://18.139.222.33:9000/',
+  serverUrl: 'http://localhost:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
-	    'sonar.projectKey':'NodeJsMSS',
+	    'sonar.projectKey':'NodeJsVemo',
 	    'sonar.login': 'admin',
-	    'sonar.password': 'admin',
+	    'sonar.password': 'Mouni0536@',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
